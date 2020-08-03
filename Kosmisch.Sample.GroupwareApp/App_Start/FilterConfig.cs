37f1caf43c8aab50eda2428922a14916c6bf8b61
健一郎 木村 <k-kimura@alterbooth.com>
2020-08-03 17:33:39 +0900

    add log filter

--- Kosmisch.Sample.GroupwareApp/App_Start/FilterConfig.cs
+++ Kosmisch.Sample.GroupwareApp/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace Kosmisch.Sample.GroupwareApp
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new Filters.LogFilter());
         }
     }
 }
---
+++ Kosmisch.Sample.GroupwareApp/Filters/LogFinter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kosmisch.Sample.GroupwareApp.Filters
+{
+    public class LogFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var req = filterContext.HttpContext.Request;
+            Trace.WriteLine($"Action Executing.|Path={req.Path}");
+        }
+    }
+}
