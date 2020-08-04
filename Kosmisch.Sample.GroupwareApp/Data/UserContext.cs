29833267ab581e7feca80d9f72f05354e2dc3e25
健一郎 木村 <k-kimura@alterbooth.com>
2020-08-04 13:12:35 +0900

    modify context and user creation

--- Kosmisch.Sample.GroupwareApp/Data/UserContext.cs
+++ Kosmisch.Sample.GroupwareApp/Data/UserContext.cs
@@ -15,7 +15,7 @@ namespace Kosmisch.Sample.GroupwareApp.Data
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
 
-        public UserContext() : base("name=UserContext")
+        public UserContext() : base("name=DefaultConnection")
         {
         }
 
